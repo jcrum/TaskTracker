5c0052a0d1fb3a55ffafdca56342eb39fcc7390d
jcrum <me@jasonmcrum.com>
2025-01-19 19:16:01 -0900

    ADD external configuration file to application

--- app/main.rb
+++ app/main.rb
@@ -1,14 +1,20 @@
 require 'sinatra/base'
+require 'sinatra/config_file'
 
 module Api
   class Main < ::Sinatra::Base
+    register Sinatra::ConfigFile
+
+
     configure :development do
       set :host_authorization, { permitted_hosts: [] }
     end
+
+    config_file File.join(File.dirname(__FILE__), '../config/config.yml')
    
     get '/' do
-      "Welcome to Task Tracker"
+      "Welcome to #{settings.app_name}"
     end
 
-  end # end module MainController
+  end # end module Main
 end # end module Api
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe 'Api::Main' do
   it 'responds to GET / with the welcome message' do
     get '/'
     expect(last_response).to be_ok
-    expect(last_response.body).to include('Welcome to Task Tracker')
+    expect(last_response.body).to include("Welcome to #{app.settings.app_name}")
   end
 end
