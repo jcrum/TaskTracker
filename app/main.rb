3768622c292a5276256c00dbf79e0cb43b3ca359
jcrum <me@jasonmcrum.com>
2025-01-27 15:34:30 -0900

    FIX RC Complaints on Main

--- app/main.rb
+++ app/main.rb
@@ -1,12 +1,16 @@
+# frozen_string_literal: true
+
 require 'sinatra/base'
 
 require_relative 'environment_config'
 require_relative 'controllers/general'
 
 module Api
+  # Main "Controller for the Applications"
   class Main < ::Sinatra::Base
+    # Include all the environment configurations
     register EnvironmentConfig
+    # Include the General/Base contoller
     register Controllers::General
-
   end # end module Main
 end # end module Api
